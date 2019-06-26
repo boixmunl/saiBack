485492277f34e2f6f86846153699ae5bf07ebe57
Lluc Boix Mundó <llucboix@gmail.com>
2019-06-26 19:41:11 +0200

    [saiBack] - Connect to ngrok, telegram bot

--- arduinoTemperature.js
+++ arduinoTemperature.js
@@ -1,15 +1,61 @@
 "use strict";
 
 var express = require('express');
+const ngrok = require('ngrok');
 var http = require('http');
+const TelegramBot = require('node-telegram-bot-api');
+const token = '799864144:AAFYArmIUiBmZaUCqxaolRA5Taj3Gnnogds';
+const bot = new TelegramBot(token, {polling: true});
 var app = express();
 var arduinoSerialPort = '/dev/ttyUSB0';	//Serial port over USB connection between the Raspberry Pi and the Arduino
-app.get('/users', (req, res) => {
-	res.send(cell)
+var restUrl;
+var sshUrl;
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+	// 'msg' is the received Message from Telegram
+	// 'match' is the result of executing the regexp above on the text content
+	// of the message
+  
+	const chatId = msg.chat.id;
+	const resp = match[1]; // the captured "whatever"
+  
+	// send back the matched "whatever" to the chat
+	bot.sendMessage(chatId, restUrl);
   });
 
+//Connect ngrok to rest API
+(async function () {
+    restUrl = await ngrok.connect({
+		proto : 'http',
+		addr : 8001,
+		authtoken : '22Ywb26viDtAwYjnouhBp_3CmqyRhsJ1tWC3qxzRbwq'
+	});
+    console.log("Ngrok connected [localhost:8001] at url: ["+restUrl+"]");
+})();
+
+//Connect ngrok to SSH
+(async function () {
+    sshUrl = await ngrok.connect({
+		proto : 'http',
+		addr : 22,
+		authtoken : '22Ywb26viDtAwYjnouhBp_3CmqyRhsJ1tWC3qxzRbwq'
+	});
+    console.log("Ngrok connected [localhost:22] at url: ["+sshUrl+"]");
+})();
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+
+app.get('/data', (req, res) => {
+	res.send(cell);
+});
+
 app.get('/', (req, res) => {
-	res.status(200).send("Welcome to API REST")
+	res.status(200).send("Welcome to cell "+restUrl)
   })
 
 http.createServer(app).listen(8001, () => {
@@ -36,27 +82,14 @@ var dataS = NaN;
 
 var cell = NaN;
 
-//var querystring = require('querystring');
 parser.on('data', function (data)
 {//When a new line of text is received from Arduino over USB
 	try
 	{
-		/* var j = JSON.parser(data);
-		dataS = data;
-		bat = j.bat / 100.0;
-		temp = j.temp / 100.0;
-		hum = j.hum / 100.0;
-		lat = j.lat / 100.0;
-		long = j.long / 100.0;
-		alt = j.alt / 100.0;
-		raw = j.raw / 100.0;
-		rzero = j.rzero / 100.0;
-		ppm = j.ppm / 100.0;*/
 		var myJsonObject = JSON.parse(data); //change to obj
 		myJsonObject.dateLastInfo = new Date(); //add something
+		myJsonObject.id = restUrl;
 		data = JSON.stringify(myJsonObject);
-		//writeFile('{"bat":"' + bat + '","temp":"' + temp + '","hum":"' + hum + '","lat":"' + lat + '","long":"' + long + '","alt":"' + alt + '","raw":"' + raw + '","rzero":"' + rzero + '","ppm":"' + ppm + '"}');
-		//postData(querystring.stringify(j));	//Forward the Arduino information to another Web server
 		cell=data;
 	}
 	catch (ex)
