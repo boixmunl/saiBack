652a52dbb7927b410adaf2dcf794858fcdacdbf3
Lluc Boix Mundó <llucboix@gmail.com>
2019-08-11 20:57:27 +0200

    sqlite

--- arduinoTemperature.js
+++ arduinoTemperature.js
@@ -10,6 +10,9 @@ var app = express();
 var proxy = require('express-http-proxy');
 var arduinoSerialPort = '/dev/ttyACM0';	//Serial port over USB connection between the Raspberry Pi and the Arduino
 var CryptoJS = require('node-cryptojs-aes').CryptoJS;
+var sqlite3 = require('sqlite3');
+var fs = require('fs');
+var db = new sqlite3.Database('./piBat.db');
 const key = "d6F3Efeq";
 const isEncrypt = true;
 
@@ -118,3 +121,71 @@ const encrypt = (text) => {
 			console.log(ex);
 	}
 }
+
+function insertBat(data){
+	// data is a javascript object   
+	var statement = db.prepare("INSERT INTO battery_records VALUES (?, ?)");
+	// Insert values into prepared statement
+	statement.run(data.battery_record[0].unix_time, data.battery_record[0].celsius);
+	// Execute the statement
+	statement.finalize();
+}
+
+function readBat(callback){
+	fs.readFile('/sys/bus/w1/devices/28-00000400a88a/w1_slave', function(err, buffer)
+ {
+		 if (err){
+				console.error(err);
+				process.exit(1);
+		 }
+
+		 // Read data from file (using fast node ASCII encoding).
+		 var data = buffer.toString('ascii').split(" "); // Split by space
+
+		 // Extract battery from string and divide by 1000 to give celsius
+		 var bat  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
+
+		 // Round to one decimal place
+		 bat = Math.round(bat * 10) / 10;
+
+		 // Add date/time to battery
+		var data = {
+					 battery_record:[{
+					 unix_time: Date.now(),
+					 charge: bat
+					 }]};
+
+		 // Execute call back with data
+		 callback(data);
+	});
+};
+
+function logBat(interval){
+	// Call the readBat function with the insertBat function as output to get initial reading
+	readBat(insertBat);
+	// Set the repeat interval (milliseconds). Third argument is passed as callback function to first (i.e. readTemp(insertTemp)).
+	setInterval(readBat, interval, insertBat);
+};
+
+function selectBat(num_records, start_date, callback){
+	// - Num records is an SQL filter from latest record back trough time series, 
+	// - start_date is the first date in the time-series required, 
+	// - callback is the output function
+	var current_bat = db.all("SELECT * FROM (SELECT * FROM battery_records WHERE unix_time > (strftime('%s',?)*1000) ORDER BY unix_time DESC LIMIT ?) ORDER BY unix_time;", start_date, num_records,
+		 function(err, rows){
+				if (err){
+				response.writeHead(500, { "Content-type": "text/html" });
+				response.end(err + "\n");
+				console.log('Error serving querying database. ' + err);
+				return;
+						 }
+				data = {battery_record:[rows]}
+				callback(data);
+	});
+};
+
+// Start battery logging (every 5 min).
+var msecs = (60 * 5) * 1000; // log interval duration in milliseconds
+logBat(msecs);
+// Send a message to console
+console.log('Server is logging to database at '+msecs+'ms intervals');
